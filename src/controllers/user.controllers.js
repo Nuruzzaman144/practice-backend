8f4c2dbab840cbe6cfdf569901c2a24d6c844683
Md Nuruzzaman <nuruzz1444@gmail.com>
2026-04-19 16:09:43 +0530

    change

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -208,7 +208,7 @@ const refreshAccessToken=asyncHandler(async(req,res)=>{
 
   try {
     const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
       throw new ApiError(401,"Unauthorized request")
     }
   
